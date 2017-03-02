d1ea51ea089674a1c9e3f2f71051618aafef54c4
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2017-03-02 16:30:23 +0100

    Staging deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for current version of Capistrano
-lock "3.7.2"
+lock '3.7.2'
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, 'lapin'
+set :repo_url, 'jonathan@127.0.0.1:/home/jonathan/temp/cdpn/cap/git/'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, '/home/jonathan/temp/cdpn/cap/remote'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -31,3 +31,13 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+namespace :deploy do
+    after :updated, :composer do
+        on roles(:web) do
+            within release_path do
+                execute :composer, :install
+            end
+        end
+    end
+end
